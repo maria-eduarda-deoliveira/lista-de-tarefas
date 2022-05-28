cee7f5343d5f3b0c9b02285af47fc227ddb47f89
Maria Eduarda de Oliveira Garcia <ol.mariae@outlook.com>
2022-05-28 20:20:02 -0300

    Updates

---
+++ precache-manifest.8db73bffbfd107700637cd01b87dd1c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b164e27741faf6683acc",
+    "url": "/lista-de-tarefas/static/js/main.ae4c0947.chunk.js"
+  },
+  {
+    "revision": "fbb1b0fe63695093254a",
+    "url": "/lista-de-tarefas/static/js/runtime~main.93ccf896.js"
+  },
+  {
+    "revision": "05ab265b4181299c0317",
+    "url": "/lista-de-tarefas/static/js/2.36af6d47.chunk.js"
+  },
+  {
+    "revision": "2c37e024265b5dd3706d63e1b4c5fc54",
+    "url": "/lista-de-tarefas/static/media/lixo.2c37e024.svg"
+  },
+  {
+    "revision": "c3b438e2e10e8df8470a437c9e4f408c",
+    "url": "/lista-de-tarefas/static/media/add.c3b438e2.svg"
+  },
+  {
+    "revision": "f60194b34c0b8e70122efcd47ba36e7a",
+    "url": "/lista-de-tarefas/static/media/code.f60194b3.svg"
+  },
+  {
+    "revision": "fde3b3a2259ee950abf336dc9016a5f0",
+    "url": "/lista-de-tarefas/index.html"
+  }
+];
